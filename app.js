df4bdf390c4467b99fc758bf1f6fcf0b317ddd9e
JulzPerez <julietoperez@gmail.com>
2022-08-14 13:11:52 +0800

    test api 3

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const userRouter = require("./api/users/userRouter");
 app.use(express.json());
 app.use("/api/users", userRouter);
 
-app.get("/test", (req,res)=>{
+/* app.get("/test", (req,res)=>{
     res.json({
         success: 1, 
         message: "Rest API is working"
     });
-});  
+}); */  
 
 app.listen(process.env.APP_PORT, ()=> {
     console.log("Server is up and running on PORT=", process.env.APP_PORT);
